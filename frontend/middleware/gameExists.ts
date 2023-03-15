917ccc038e189bdd13807464877c9780ada97413
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2023-03-15 09:29:12 -0600

    [frontend]: fixed gameExists to use baseURL

--- frontend/middleware/gameExists.ts
+++ frontend/middleware/gameExists.ts
@@ -1,19 +1,19 @@
 import { useGameStore } from "~/stores/useGameStore";
 import { useFetch } from "#app";
 import { components } from "~/schema";
+import { useConfig } from "~/composeables/useConfig";
 
 export default defineNuxtRouteMiddleware(async (to) => {
     const { id } = to.params;
     const { setGame, game } = useGameStore();
+    const { baseURL } = useConfig();
 
     if (game?.id === id) return true;
 
-    const { data, error } = await useFetch(
-        `http://127.0.0.1:8000/api/game/${id}`,
-        {
-            method: "GET",
-        }
-    );
+    const { data, error } = await useFetch(`/api/game/${id}`, {
+        method: "GET",
+        baseURL,
+    });
     if (!data.value || error.value) {
         return abortNavigation();
     }
