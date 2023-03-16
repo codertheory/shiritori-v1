bf4f253428ab8c914fb27c9a1f2eacd6f0553067
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2023-03-15 21:53:37 -0600

    [backend]: Updated update_dictionary command to batch insert.

--- backend/shiritori/game/management/commands/update_dictionary.py
+++ backend/shiritori/game/management/commands/update_dictionary.py
@@ -3,6 +3,11 @@ from django.core.management import BaseCommand
 from shiritori.game.models import Word
 
 
+def chunk_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
 class Command(BaseCommand):
     help = 'Updates the word dictionary'
 
@@ -15,6 +20,8 @@ class Command(BaseCommand):
             with open(f'dictionaries/{locale}.txt', 'r', encoding='utf-8') as f:
                 words = f.read().splitlines()
             self.stdout.write(self.style.HTTP_INFO(f'Updating {locale} dictionary with {len(words)} words'))
-            results = Word.objects.bulk_create([Word(word=word, locale=locale) for word in words],
-                                               ignore_conflicts=True)
-            self.stdout.write(self.style.SUCCESS(f'Successfully updated {locale} dictionary with {len(results)} words'))
+            # Batch insert the words into the database in chunks of 1000
+            for chunk in chunk_list(words, 1000):
+                Word.objects.bulk_create([Word(word=word, locale=locale) for word in chunk], batch_size=1000,
+                                         ignore_conflicts=True)
+            self.stdout.write(self.style.SUCCESS(f'Successfully updated {locale} dictionary with {len(words)} words'))
