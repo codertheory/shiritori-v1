99b72249a1f7ac898f3071ab739b4573aed738ed
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2023-03-16 18:26:54 -0600

    [backend]: Changed Celery env var to REDIS_URL

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -229,7 +229,7 @@ if USE_TZ:
     # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-timezone
     CELERY_TIMEZONE = TIME_ZONE
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://localhost:6379/0")
+CELERY_BROKER_URL = env("REDIS_URL", default="redis://localhost:6379/0")
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_backend
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-extended
