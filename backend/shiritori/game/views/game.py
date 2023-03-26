7412a6daa039dad2068118e1b74c900ebf9b376e
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2023-03-25 20:13:41 -0600

    [backend] - lint

--- backend/shiritori/game/views/game.py
+++ backend/shiritori/game/views/game.py
@@ -20,9 +20,7 @@ from shiritori.game.serializers import (
 )
 from shiritori.game.tasks import game_worker_task
 
-__all__ = (
-    "GameViewSet",
-)
+__all__ = ("GameViewSet",)
 
 
 class GameViewSet(ReadOnlyModelViewSet):
